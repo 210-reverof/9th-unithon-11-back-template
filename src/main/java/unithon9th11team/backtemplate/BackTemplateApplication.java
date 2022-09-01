085b09dbb53da51980e37fa044f604d6c4557970
210_reverof <sheltonwon@gmail.com>
2022-09-01 21:56:32 +0900

    [ADD] springboot project 추가

---
+++ src/main/java/unithon9th11team/backtemplate/BackTemplateApplication.java
@@ -0,0 +1,13 @@
+package unithon9th11team.backtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unithon9th11team/backtemplate/BackTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package unithon9th11team.backtemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
